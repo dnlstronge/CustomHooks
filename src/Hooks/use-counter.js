d7a6e39c4fb4b4a9ba77dec6e4dddf6e1ac8bb5b
Dan Stronge <dnlstronge@gmail.com>
2023-04-04 08:42:49 +0100

    [x] - refactor

--- src/Hooks/use-counter.js
+++ src/Hooks/use-counter.js
@@ -8,16 +8,17 @@ const [counter, setCounter] = useState(0);
         useEffect(() => {
         if(forwards === true) {
           const interval = setInterval(() => {
-            setCounter((prevCounter) => prevCounter + 1);
+            if(forwards) {
+            setCounter((prevCounter) => prevCounter + 1);}
+            if(!forwards) {
+            setCounter((prevCounter) => prevCounter - 1)
+            }
           }, 1000);
           return () => clearInterval(interval);
-        } else { 
-          const interval = setInterval(() => {
-            setCounter((prevCounter) => prevCounter -1)
-          }) 
-          return () => clearInterval(interval);}
+        } 
+       
           
-        }, []);
+        }, [forwards]);
         return counter
     }
 export default useCounter;
