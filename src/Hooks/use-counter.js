fdedecf26839ba3129e846ce4f7fd76a2c86ebce
Dan Stronge <dnlstronge@gmail.com>
2023-04-04 08:39:50 +0100

    [x] - add conditional logic to useEffect

--- src/Hooks/use-counter.js
+++ src/Hooks/use-counter.js
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react";
 
 
-const useCounter = () => {
+const useCounter = (forwards = true) => {
     
 const [counter, setCounter] = useState(0);
       
         useEffect(() => {
+        if(forwards === true) {
           const interval = setInterval(() => {
             setCounter((prevCounter) => prevCounter + 1);
           }, 1000);
-      
           return () => clearInterval(interval);
+        } else { 
+          const interval = setInterval(() => {
+            setCounter((prevCounter) => prevCounter -1)
+          }) 
+          return () => clearInterval(interval);}
+          
         }, []);
-        return counter;
+        return counter
     }
 export default useCounter;
