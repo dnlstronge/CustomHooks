89ad4655ef44f42008e83e909538a4215cb146a2
Dan Stronge <dnlstronge@gmail.com>
2023-04-04 08:48:50 +0100

    [x] - minor bug fix

--- src/Hooks/use-counter.js
+++ src/Hooks/use-counter.js
@@ -1,24 +1,18 @@
 import { useEffect, useState } from "react";
 
-
 const useCounter = (forwards = true) => {
-    
-const [counter, setCounter] = useState(0);
-      
-        useEffect(() => {
-        if(forwards === true) {
-          const interval = setInterval(() => {
-            if(forwards) {
-            setCounter((prevCounter) => prevCounter + 1);}
-            else {
-            setCounter((prevCounter) => prevCounter - 1)
-            }
-          }, 1000);
-          return () => clearInterval(interval);
-        } 
-       
-          
-        }, [forwards]);
-        return counter
-    }
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [forwards]);
+  return counter;
+};
 export default useCounter;
